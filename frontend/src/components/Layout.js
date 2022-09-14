fdef3982f395accd6a8002160a16d99776071853
fikrianggara <221810306@stis.ac.id>
2022-09-15 01:40:34 +0700

    initiate frontend, add routes, and pages

---
+++ frontend/src/components/Layout.js
@@ -0,0 +1,11 @@
+import { Outlet } from "react-router-dom";
+
+import React from "react";
+
+//general parent component
+// that act as the general UI layout of the app
+const Layout = () => {
+  return <Outlet />;
+};
+
+export default Layout;
