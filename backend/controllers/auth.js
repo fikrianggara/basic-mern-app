c59d0c47849918e4de14816a4f5d781ce05a0558
fikrianggara <221810306@stis.ac.id>
2022-09-23 23:34:21 +0700

    add auth API slice on frontend

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -30,7 +30,7 @@ const login = asyncHandler(async (req, res) => {
       },
     },
     process.env.ACCESS_TOKEN_SECRET,
-    { expiresIn: "1m" }
+    { expiresIn: "1d" }
   );
   const refreshToken = jwt.sign(
     {
@@ -75,7 +75,7 @@ const refresh = (req, res) => {
           },
         },
         process.env.ACCESS_TOKEN_SECRET,
-        { expiresIn: "1m" }
+        { expiresIn: "1d" }
       );
       res.json({ accessToken });
     })
