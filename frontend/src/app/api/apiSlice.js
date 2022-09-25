0d52b055025b6c530a5a56d255884c131cf911ff
fikrianggara <221810306@stis.ac.id>
2022-09-26 01:35:40 +0700

    add base query with reauthentication

--- frontend/src/app/api/apiSlice.js
+++ frontend/src/app/api/apiSlice.js
@@ -1,5 +1,5 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
-
+import { setCredentials } from "../../features/auth/authSlice";
 //used as a wrapper to simplify request
 const baseQuery = fetchBaseQuery({
   baseUrl: "http://localhost:3000",
@@ -13,9 +13,34 @@ const baseQuery = fetchBaseQuery({
     return headers;
   },
 });
+
+//re authenticate jwt with refresh token to get new access token
+const baseQueryWithReauth = async (args, api, extraOptions) => {
+  let result = await baseQuery(args, api, extraOptions);
+
+  if (result?.error?.status === 403) {
+    console.log("sendirng regresh token");
+    //refresh the token
+    const refreshResult = await baseQuery("/auth/refresh", api, extraOptions);
+
+    if (refreshResult?.data) {
+      //set new access token
+      api.dispatch(setCredentials({ ...refreshResult.data }));
+
+      //retry original query with new accessToken
+      result = await baseQuery(args, api, extraOptions);
+    } else {
+      if (refreshResult?.error?.status === 403) {
+        refreshResult.error.data.message = "Your login session has expired";
+      }
+      return refreshResult;
+    }
+  }
+  return result;
+};
 // general api endpoint, notes and user endpoint will extends this apiSlice.
 export const apiSlice = createApi({
-  baseQuery,
+  baseQuery: baseQueryWithReauth,
   tagTypes: ["Note", "User"],
   endpoints: (builder) => ({}),
 });
