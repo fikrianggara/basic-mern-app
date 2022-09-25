f470e6b984095053f92340d6bc1b0d59f59e8a34
fikrianggara <221810306@stis.ac.id>
2022-09-25 23:14:48 +0700

    setup basequery as request wrapper

--- frontend/src/app/api/apiSlice.js
+++ frontend/src/app/api/apiSlice.js
@@ -1,8 +1,21 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
+//used as a wrapper to simplify request
+const baseQuery = fetchBaseQuery({
+  baseUrl: "http://localhost:3000",
+  credentials: "include",
+  prepareHeaders: (headers, { getState }) => {
+    //set a authorization token in headers if there is
+    const token = getState().auth.token;
+    if (token) {
+      headers.set("authorization", `Bearer ${token}`);
+    }
+    return headers;
+  },
+});
 // general api endpoint, notes and user endpoint will extends this apiSlice.
 export const apiSlice = createApi({
-  baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:3000" }),
+  baseQuery,
   tagTypes: ["Note", "User"],
   endpoints: (builder) => ({}),
 });
